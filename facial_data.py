af495c60f47b6ee5efdbf88d60f340d2195ca2f1
MITHESHMANAS <nmithesh06@gmail.com>
2026-05-19 22:08:38 +0530

    redesigned dataset generation module with dynamic storage handling

--- facial_data.py
+++ facial_data.py
@@ -1,64 +1,62 @@
-    import cv2
-    import numpy as np 
+# facial_data.py
 
-    cap = cv2.VideoCapture(0)
-    face_cascade = cv2.CascadeClassifier("data.xml")
+import cv2
+import os
 
-    skip = 0
-    face_data = []
-    dataset_path = "./face_dataset/"
+face_classifier = cv2.CascadeClassifier("data.xml")
 
-    file_name = input("Enter the name of person : ")
+name = input("Enter the name of person : ")
 
+path = "datasets/" + name
 
-    while True:
-        ret,frame = cap.read()
+if not os.path.exists(path):
+    os.makedirs(path)
 
-        gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+camera = cv2.VideoCapture(0)
 
-        if ret == False:
-            continue
+count = 0
 
-        faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
-        if len(faces) == 0:
-            continue
+while True:
 
-        k = 1
+    ret, frame = camera.read()
 
-        faces = sorted(faces, key = lambda x : x[2]*x[3] , reverse = True)
+    if not ret:
+        break
 
-        skip += 1
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        for face in faces[:1]:
-            x,y,w,h = face
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
 
-            offset = 5
-            face_offset = frame[y-offset:y+h+offset,x-offset:x+w+offset]
-            face_selection = cv2.resize(face_offset,(100,100))
+    for (x, y, w, h) in faces:
 
-            if skip % 10 == 0:
-                face_data.append(face_selection)
-                print (len(face_data))
+        count += 1
 
+        face = frame[y:y+h, x:x+w]
 
-            cv2.imshow(str(k), face_selection)
-            k += 1
-            
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        face = cv2.resize(face, (200, 200))
 
-        cv2.imshow("faces",frame)
+        file_name = path + "/" + str(count) + ".jpg"
 
-        key_pressed = cv2.waitKey(1) & 0xFF
-        if key_pressed == ord('q'):
-            break
+        cv2.imwrite(file_name, face)
 
-    face_data = np.array(face_data)
-    face_data = face_data.reshape((face_data.shape[0], -1))
-    print (face_data.shape)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
+        cv2.putText(
+            frame,
+            f"Captured : {count}",
+            (20,40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,0),
+            2
+        )
 
-    np.save(dataset_path + file_name, face_data)
-    print ("Dataset saved at : {}".format(dataset_path + file_name + '.npy'))
+    cv2.imshow("Face Dataset Creator", frame)
 
-    cap.release()
-    cv2.destroyAllWindows()
+    if cv2.waitKey(1) == 13 or count == 100:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+
+print("Dataset collection completed successfully")
